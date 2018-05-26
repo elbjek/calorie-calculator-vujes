3eea777cb5597ab8a2528e786f1e23388b5d58e7
elbjek <lenabjekovic@hotmaill.com>
2018-05-26 17:24:01 +0200

    end me

--- scripts/domain/calorieCounter.js
+++ scripts/domain/calorieCounter.js
@@ -1,19 +1,18 @@
-'use strict';
 
 var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Vue!',
-    info: ''
+    info:''
   },
-  mounted: function mounted() {
-    var _this = this;
-
-    axios.get('http://api.myjson.com/bins/m4pk6').then(function (response) {
-      return _this.info = response.data.mcdonalds;
-    });
+  mounted () {
+    axios
+      .get('http://api.myjson.com/bins/m4pk6')
+      .then(response => (this.info = response.data.mcdonalds))
   }
+  
 });
+
 
 //api http://api.myjson.com/bins/61ksm
 // [{"CAL":"740","FAT":"41","SFAT":"16","TFAT":"1.5","CHOL":"125","SALT":"1480","CARB":"51","FBR":"4","SGR":"14","PRO":"40","ITEM":"Bacon Clubhouse Burger 9.7 oz (274 g)","CATEGORY":"BURGERSANDWICH"},
