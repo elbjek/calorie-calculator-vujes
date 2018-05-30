b4f9eed041f4491690a5656ee640d2d191881946
elbjek <lenabjekovic@hotmaill.com>
2018-05-30 22:24:11 +0200

    select element

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,91 +1,34 @@
 'use strict';
 
-Vue.component('tabs', {
-
-  template: '\n  <div class="tab">\n  <ul>\n  <li  v-for="tab in tabs" :class="{\'is-active\':tab.isActive}"> <a href="#" @click="selectTab(tab)" > {{ tab.name }} </a></li>\n  </ul>\n  <div>\n  <slot></slot>\n  </div>\n  </div>\n  ',
-  data: function data() {
-    return {
-      tabs: []
-    };
-  },
-  created: function created() {
-    this.tabs = this.$children;
-  },
-
-  methods: {
-    selectTab: function selectTab(selectedTab) {
-      this.tabs.forEach(function (tab) {
-        tab.isActive = tab.name == selectedTab.name;
-      });
-    }
-  }
-
-});
-
-Vue.component('tab', {
-
-  template: '\n  \n  <div v-show="isActive"><slot></slot></div>\n  \n  ',
-
-  props: {
-    name: { required: true },
-    selected: { default: false }
-  },
-  data: function data() {
-    return {
-      isActive: false
-    };
-  },
-  mounted: function mounted() {
-    this.isActive = this.selected;
-  }
-});
-
-Vue.component('counter', {
-  template: ' \n  <div class="box" >\n\t<h2>00</h2>\n  <p>CAL</p>\n  </div>\n  ',
-  props: ['cal']
-});
-
 var app = new Vue({
   el: '#app',
   data: {
     info: '',
-    burgers: [],
-    sandwiches: [],
-    sides: [],
-    breakfast: [],
-    cal: []
+    selectedItems: []
+
   },
   mounted: function mounted() {
     var _this = this;
 
-    axios.get('http://api.myjson.com/bins/m4pk6').then(function (response) {
-      return (_this.info = response.data.mcdonalds).then(_this.getItem());
+    axios.get('http://api.myjson.com/bins/61ksm').then(function (response) {
+      return _this.info = response.data;
     });
   },
 
   methods: {
-    getItem: function getItem() {
-      for (var i = 0; i < this.info.length; i++) {
-        // GET ITEMS
-        if (this.info[i].CATEGORY == 'BURGERSANDWICH') {
+    getItem: function getItem(item) {
 
-          this.burgers.push(this.info[i].ITEM);
-        } else if (this.info[i].CATEGORY == 'CHICKENFISH') {
-
-          this.sandwiches.push(this.info[i].ITEM);
-        } else if (this.info[i].CATEGORY == 'BREAKFAST') {
-
-          this.breakfast.push(this.info[i].ITEM);
-        } else if (this.info[i].CATEGORY == 'SNACKSIDE') {
-
-          this.sides.push(this.info[i].ITEM);
-        } else if (this.info[i].CAL) {
-          this.cal.push(this.info[i].CAL);
-        }
+      //SOME STACKOVERFLOW FUNCTION I DONT UNDERSTAND
+      function getEventTarget(e) {
+        e = e || window.event;
+        return e.target || e.srcElement;
       }
-    },
-    thisFucc: function thisFucc() {
-      alert('ay');
+
+      var ul = document.getElementById('burger');
+      ul.onclick = function (event) {
+        var target = getEventTarget(event);
+        alert(target.innerHTML);
+      };
     }
   }
 });
--- scripts/domain/calorieCounter.js
+++ scripts/domain/calorieCounter.js
@@ -1,120 +1,37 @@
 
-Vue.component('tabs',{
-  
-  template:`
-  <div class="tab">
-  <ul>
-  <li  v-for="tab in tabs" :class="{'is-active':tab.isActive}"> <a href="#" @click="selectTab(tab)" > {{ tab.name }} </a></li>
-  </ul>
-  <div>
-  <slot></slot>
-  </div>
-  </div>
-  `,
-  data(){
-    return {
-      tabs:[]
-    }
-  },
-  created(){
-    this.tabs = this.$children;
-  },
-  methods:{
-    selectTab(selectedTab){
-      this.tabs.forEach(tab=>{
-        tab.isActive = (tab.name == selectedTab.name)
-      })
-    }
-  }
-  
-});
-
-Vue.component('tab',{
-  
-  template:`
-  
-  <div v-show="isActive"><slot></slot></div>
-  
-  `,
-  
-  props:{
-    name: {required:true },
-    selected: {default : false}
-  },
-  data(){
-    return {
-      isActive:false
-    }
-  },
-  mounted(){
-    this.isActive = this.selected
-  }
-})
-
-Vue.component('counter',{
-  template:` 
-  <div class="box" >
-	<h2>00</h2>
-  <p>CAL</p>
-  </div>
-  `,
-  props:['cal']
-})
-
-
 
 var app = new Vue({
   el: '#app',
   data: {
     info:'',
-    burgers:[],
-    sandwiches:[],
-    sides:[],
-    breakfast:[],
-    cal:[]
+    selectedItems:[],
+    
   },
   mounted () {
-    axios
-    .get('http://api.myjson.com/bins/m4pk6')
-    .then(response => (this.info = response.data.mcdonalds)
-    .then(this.getItem())
     
-  )
-},
-methods:{
-  getItem:function(){
-    for(var i = 0; i<this.info.length; i++){
-      // GET ITEMS
-      if(this.info[i].CATEGORY=='BURGERSANDWICH' ){
-        
-        this.burgers.push(this.info[i].ITEM);
-  
-        
-      }
-      else if (this.info[i].CATEGORY=='CHICKENFISH'){
-        
-        this.sandwiches.push(this.info[i].ITEM)
-        
-      }
-       else if(this.info[i].CATEGORY=='BREAKFAST' ){
-        
-        this.breakfast.push(this.info[i].ITEM)
-        
-      }else if(this.info[i].CATEGORY=='SNACKSIDE' ){
-        
-        this.sides.push(this.info[i].ITEM);
-        
-      } 
-      else if(this.info[i].CAL ){
-        this.cal.push(this.info[i].CAL);
-      } 
-    }
+    axios
+    .get('http://api.myjson.com/bins/61ksm')
+    .then(response => (this.info = response.data))
+    
+    
+    
   },
-  thisFucc(){
-    alert('ay')
+  methods:{
+    getItem(item){
+
+      //SOME STACKOVERFLOW FUNCTION I DONT UNDERSTAND
+      function getEventTarget(e) {
+        e = e || window.event;
+        return e.target || e.srcElement; 
+      }
+      
+      var ul = document.getElementById('burger');
+      ul.onclick = function(event) {
+        var target = getEventTarget(event);
+        alert(target.innerHTML);
+      };
+    }
   }
-  
-}
 });
 
 
